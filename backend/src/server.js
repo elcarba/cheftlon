58372497fa2ae9d42cc8caf2247fa8812116f3a9
luiscarbajal <luisf144@hotmail.com>
2021-02-09 09:26:40 +0000

    bug: backend prod

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,17 +28,7 @@ app.use(cors());
 // Define Routes
 app.use('/api', indexRoute);
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
 
